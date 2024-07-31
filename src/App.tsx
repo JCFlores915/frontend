549cf82544428898bc46e3388c44fdae65e37bdf
jflores_mvapp <juan@logacode.com>
2024-07-31 16:14:29 -0600

    chore: Add apollo-upload-client dependency and update ApolloClient configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useMutation, gql, ApolloProvider } from '@apollo/client';
+import { Upload, Button, Input, Form } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import ApolloClient from './apollo-client';
+import { RcFile } from 'antd/lib/upload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const UPLOAD_PHOTO = gql`
+    mutation uploadPhoto($name: String!, $file: Upload!) {
+        uploadPhoto(name: $name, file: $file) {
+            id
+            name
+            photoUrl
+        }
+    }
+`;
 
-export default App;
+const App: React.FC = () => {
+    const [name, setName] = useState('');
+    const [file, setFile] = useState<RcFile | null>(null);
+    const [uploadPhoto] = useMutation(UPLOAD_PHOTO);
+
+    const handleUpload = async () => {
+        if (file) {
+            const { data } = await uploadPhoto({
+                variables: {
+                    name,
+                    file
+                }
+            });
+            console.log(data);
+        }
+    };
+
+    return (
+        <Form>
+            <Form.Item label="Name">
+                <Input value={name} onChange={(e) => setName(e.target.value)} />
+            </Form.Item>
+            <Form.Item label="Upload Photo">
+                <Upload
+                    beforeUpload={(file) => {
+                        setFile(file as RcFile);
+                        return false;
+                    }}
+                    accept="image/*"
+                >
+                    <Button icon={<UploadOutlined />}>Select File</Button>
+                </Upload>
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" onClick={handleUpload}>Upload</Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+const WrappedApp = () => (
+    <ApolloProvider client={ApolloClient}>
+        <App />
+    </ApolloProvider>
+);
+
+export default WrappedApp;
---
+++ src/apollo-client.ts
@@ -0,0 +1,10 @@
+// src/apollo-client.ts
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
+
+const client = new ApolloClient({
+    link: createUploadLink({ uri: 'http://localhost:4000/graphql' }),
+    cache: new InMemoryCache()
+});
+
+export default client;
